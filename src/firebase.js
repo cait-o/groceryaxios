ae8afc3b6d2676bb567187fc3429a1570939a60b
caitlin <caitlinong@gmail.com>
2020-03-24 11:49:26 +0800

    new database

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-    apiKey: "AIzaSyByuFhJglTG0EJTvNATpGZBAiXWmaRJdFY",
-    authDomain: "axios-803df.firebaseapp.com",
-    databaseURL: "https://axios-803df.firebaseio.com",
-    projectId: "axios-803df",
-    storageBucket: "axios-803df.appspot.com",
-    messagingSenderId: "195609141993",
-    appId: "1:195609141993:web:255edabd9659aa0d688678",
-    measurementId: "G-68SBFX5DW2"
+    apiKey: "AIzaSyBoXsnO1yE5HAvjQFLxtR9MgkOl67YA584",
+    authDomain: "axios-2-2c328.firebaseapp.com",
+    databaseURL: "https://axios-2-2c328.firebaseio.com",
+    projectId: "axios-2-2c328",
+    storageBucket: "axios-2-2c328.appspot.com",
+    messagingSenderId: "263810540541",
+    appId: "1:263810540541:web:10332ad0467130e9caef94",
+    measurementId: "G-3L69KHEKNG"
 
   };
   
